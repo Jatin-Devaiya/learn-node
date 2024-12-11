ca33d1b5681b439a5f728dc6215cc068579a6ecb
Jatin Devaiya <jatin@redefinesolutions.com>
2024-12-11 18:09:12 +0530

    crud with mongo db

--- index.jsx
+++ index.jsx
@@ -2,42 +2,86 @@ const express = require("express");
 const users = require("./MOCK_DATA.json");
 const fs = require("fs");
 const mongoose = require("mongoose");
+const { type } = require("os");
 
 const app = express();
 const PORT = 8000;
 
+//conection
+mongoose
+  .connect("mongodb://localhost:27017/app-1")
+  .then(() => console.log("Mongo DB connected"))
+  .catch((err) => console.log("mongo error", err));
+
+//schema
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      // unique: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
 // Middleware - Plugin
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/api/users", (req, res) => {
-  res.setHeader("myName", "JD"); // custum  header
-  console.log(req.headers, "req.feaders.......");
-  return res.json(users);
+app.get("/api/users", async (req, res) => {
+  const allDbUsers = await User.find({});
+  return res.json(allDbUsers);
 });
 
-app.get("/api/users/:id", (req, res) => {
+app.get("/api/users/:id", async (req, res) => {
   //get  user with id
-  const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id);
+  const user = await User.findById(req.params.id);
   if (!user) return res.status(404).json({ errro: "user not found" });
   return res.json(user);
 });
 
-app.route("/api/users/:id").delete((req, res) => {
-  //Delete the users with id
-  const id = Number(req.params.id);
-  const deleteIdx = users.findIndex((user) => user.id === id);
-
-  users.splice(deleteIdx, 1);
-
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    if (err) console.log(err);
-    console.log("works");
-    return res.json({ status: "user deleted successfully" });
-  });
+app.patch("/api/users/:id", async (req, res) => {
+  // update user with id
+  await User.findByIdAndUpdate(req.params.id, { ...req.body });
+  return res.json({ status: "Success" });
 });
 
-app.post("/api/users", (req, res) => {
+app.delete("/api/users/:id", async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  return res.json({ status: "Success" });
+});
+
+// app.route("/api/users/:id").delete((req, res) => {
+//   //Delete the users with id
+//   const id = Number(req.params.id);
+//   const deleteIdx = users.findIndex((user) => user.id === id);
+
+//   users.splice(deleteIdx, 1);
+
+//   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+//     if (err) console.log(err);
+//     console.log("works");
+//     return res.json({ status: "user deleted successfully" });
+//   });
+// });
+
+app.post("/api/users", async (req, res) => {
   //create new user
   const body = req.body;
   if (
@@ -45,21 +89,29 @@ app.post("/api/users", (req, res) => {
     !body.first_name ||
     !body.last_name ||
     !body.email ||
-    !body.gender
+    !body.gender ||
+    !body.job_title
   ) {
     return res.status(400).json({ msg: "All field are required" });
   }
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({ status: "success", id: users.length });
+
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job_title,
   });
+
+  return res.status(201).json({ msg: "User created successfully" });
 });
 
 // print HTML
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+  const allDbUsers = await User.find({});
   const html = `
         <ul>
-        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        ${allDbUsers.map((user) => `<li>${user.firstName}</li>`).join("")}
         </ul>
     `;
   res.send(html);
